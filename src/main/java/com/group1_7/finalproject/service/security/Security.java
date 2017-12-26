cc34d092000997fa1bd89798adab6653b19556c8
naberal <hmelarvova@gmail.com>
2017-12-26 03:28:45 +0200

    com

---
+++ src/main/java/com/group1_7/finalproject/service/security/Security.java
@@ -0,0 +1,6 @@
+package com.group1_7.finalproject.service.security;
+
+public class Security {
+    public void findLoggedInUsername() {}
+    public void autoLogin(String email, String password){}
+}
